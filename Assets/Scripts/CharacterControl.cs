a01ce56ed38c3b9bbff1cff253b99b8b6f8c27c0
Sefisch <sefisch@googlemail.com>
2015-07-09 15:57:22 +0200

    Bump!

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CharacterControl : MonoBehaviour {
 
-	public float maxSpeed = 4;
+	public float maxSpeed = 20;
 	public float jumpForce = 5;
 	public float gravity = 9.81f;
 	public Vector3 moveDirection = Vector3.zero; //Eine Varibale vom Typ Vector3 (x,y,z) gesetzt auf (0,0,0)
@@ -29,13 +29,19 @@ public class CharacterControl : MonoBehaviour {
 
 	void Move()
 	{
-		float velocity = Input.GetAxis ("Horizontal") * maxSpeed;
-		moveDirection.x = velocity;
+		float velocity = Input.GetAxis ("Horizontal");
+
+		moveDirection.x += velocity;
+		moveDirection.x = Mathf.Clamp (moveDirection.x, -maxSpeed, maxSpeed);
+		//moveDirection.x *= 0.98f;
 
 		if (!cc.isGrounded) {
 			moveDirection.y -= gravity * Time.deltaTime;
 			isGrounded=false;
 		} else {
+			if (Mathf.Abs(velocity) < 0.9)
+				moveDirection.x *= 0.175f;
+
 			isGrounded=true;
 			moveDirection.y = 0; //theoretisch nicht notwendig, wichtig falls jump auskommentiet wird
 			if (Input.GetAxis ("Vertical")>0.01f) {
@@ -54,6 +60,27 @@ public class CharacterControl : MonoBehaviour {
 
 
 	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		if (hit.collider.gameObject.layer != 9) return;
+		//Basically if you have a vector v, which represents the object's velocity, and a normalized normal vector n,
+		//which is perpendicular to the surface with which the object collides, then the new velocity v' is given by the equation:
+		//v' = 2 * (v . n) * n - v;
+		//Where '*' is the scalar multiplication operator, '.' is the dot product of two vectors, and '-' is the subtraction operator for two vectors. v is reflected off of the surface, and gives a reflection vector v' which is used as the new velocity of the object. 
+		Vector2 n = new Vector2 (hit.normal.x, hit.normal.y);
+		Vector2 v = new Vector2 (moveDirection.x, moveDirection.y);
+		if (n.y > 0)
+			return;
+		float dot = Vector2.Dot (v, n);
+		if (dot >= 0)
+			return;
+		Vector2 nv = v-(n*(dot*2f));
+		Debug.Log(""+n+v+nv+" "+dot);
+
+		moveDirection = new Vector3 (nv.x, nv.y, 0) * 0.75f;
+
+	}
+
 
 	public void Flip()
 	{
