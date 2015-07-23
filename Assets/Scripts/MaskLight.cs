cdf3e2f8a8cd9e35291e9e13381bce0f828adb4d
Sefisch <sefisch@googlemail.com>
2015-07-23 11:37:26 +0200

    Repair MaskLight

---
+++ Assets/Scripts/MaskLight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Light))]
+public class MaskLight : MonoBehaviour {
+
+	// Use this for initialization
+	private Light myLight;
+	public CharacterControl myCharacter;
+
+	public void TurnLightOff() {
+		myLight.intensity = 0;
+	}
+
+	public void TurnLightOn() {
+		myLight.intensity = 5;
+		myLight.color = Color.red;
+	}
+
+	void Start () {
+		myLight = GetComponent<Light> ();
+		Debug.Log (myLight);
+	}
+
+	public void LightControl() {
+
+		do {
+			myLight.intensity ++;
+		} while(Input.GetKeyDown(KeyCode.R));
+
+
+
+		if (Input.GetKeyDown(KeyCode.F)) {
+			myLight.intensity --;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myCharacter.activeMask == 2)
+			LightControl ();
+	}
+}
